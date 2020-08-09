7de3baea1004d62f0a68f8a9ba6228aff7cae398
Kevin Macias-Matsuura <kevinmaciasmatsuura@gmail.com>
2020-08-08 23:04:34 -0400

    added named entity recognition

--- project_functions.py
+++ project_functions.py
@@ -4,3 +4,21 @@ def check_for_string(data_frame, column_name, string):
         print("There are {m} instances.".format(m = count))
     else:
         print("There are no instances.")
+
+def cleanText(data_frame, column_name:str, new_column:str):
+    data_frame[column_name] = data_frame[column_name].str.replace('<br />', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace("'", '')    
+    data_frame[column_name] = data_frame[column_name].str.replace('[^\w\s]', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace('[0-9]', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace('_', ' ')
+    # Lowercase
+    data_frame[column_name] = data_frame[column_name].str.lower()
+
+def cleanColumn(data_frame, column_name:str):
+    data_frame[column_name] = data_frame[column_name].str.replace('<br />', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace("'", '')    
+    data_frame[column_name] = data_frame[column_name].str.replace('[^\w\s]', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace('[0-9]', ' ')
+    data_frame[column_name] = data_frame[column_name].str.replace('_', ' ')
+    # Lowercase
+    data_frame[column_name] = data_frame[column_name].str.lower()
