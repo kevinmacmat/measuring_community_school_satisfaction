364418a03424496a86abc2105f915c1c655ef942
Kevin Macias-Matsuura <kevinmaciasmatsuura@gmail.com>
2020-08-10 20:25:43 -0400

    added modeling notebook and sqr_rating

--- project_functions.py
+++ project_functions.py
@@ -5,20 +5,15 @@ def check_for_string(data_frame, column_name, string):
     else:
         print("There are no instances.")
 
-def cleanText(data_frame, column_name:str, new_column:str):
-    data_frame[column_name] = data_frame[column_name].str.replace('<br />', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace("'", '')    
-    data_frame[column_name] = data_frame[column_name].str.replace('[^\w\s]', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace('[0-9]', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace('_', ' ')
-    # Lowercase
-    data_frame[column_name] = data_frame[column_name].str.lower()
-
-def cleanColumn(data_frame, column_name:str):
-    data_frame[column_name] = data_frame[column_name].str.replace('<br />', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace("'", '')    
-    data_frame[column_name] = data_frame[column_name].str.replace('[^\w\s]', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace('[0-9]', ' ')
-    data_frame[column_name] = data_frame[column_name].str.replace('_', ' ')
+def cleanText(data_frame, column_name:str):
+    data_frame = data_frame
+    # Remove numbers
+    data_frame[column_name] = data_frame[column_name].str.replace(r'[0-9]', '')
+    # Remove punctuation
+    data_frame[column_name] = data_frame[column_name].str.replace(r'[^\w\s]', '')    
+    # Remove underscore
+    data_frame[column_name] = data_frame[column_name].str.replace(r'[_]', '')
+    # Remove whitespace
+    data_frame[column_name] = data_frame[column_name].str.replace(r'\t', '')
     # Lowercase
     data_frame[column_name] = data_frame[column_name].str.lower()
